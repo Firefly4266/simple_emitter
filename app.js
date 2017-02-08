5ba14824ed6a0c18f69c8c178fb19d3c91b84d2d
jp <gaines.johnp@gmail.com>
2017-02-07 16:33:35 -0800

    simple emitter with config module instead of magic string

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 'use strict';
 
-var Emitter = require('./emitter');
+var configEvent = require('./config').events
+
+var Emitter = require('events');
 
 var emtr = new Emitter();
 
-emtr.on('greet', function(){
+emtr.on('configEvent', function(){
     console.log('A greeting has been made.');
   });
 
-emtr.on('greet', function(){
+emtr.on('configEvent', function(){
   console.log('Second listener firing!');
 });
 
-emtr.emit('greet');
+emtr.emit('configEvent');
---
+++ config.js
@@ -0,0 +1,9 @@
+'use strict';
+//config.js is a file (module) that has properties set to the strings that will
+// be passed to the event emitter (like "greet")
+module.exports = {
+  events: {
+    GREET: 'greet',
+    FILE_SAVED: 'file.saved'
+  }
+}
